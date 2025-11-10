cf8644711527e5f20089adc7c1dcf4b0052ee1ee
KarimMoustamid <moustamid.teck@gmail.com>
2025-11-10 13:45:44 -0600

    feat: add initial Docker setup and update project to .NET 10.0

---
+++ Evently.API/Program.cs
@@ -0,0 +1,13 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.Run();
